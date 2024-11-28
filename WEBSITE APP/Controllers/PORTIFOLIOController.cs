3286a06fed927c1e5bf4a193b55fddc89d95446b
Micjeal <142317765+Micjeal@users.noreply.github.com>
2024-11-28 10:58:07 +0300

    Add project files.

---
+++ WEBSITE APP/Controllers/PORTIFOLIOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSITE_APP.Controllers
+{
+    public class PORTIFOLIOController : Controller
+    {
+        // GET: PORTIFOLIO
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
